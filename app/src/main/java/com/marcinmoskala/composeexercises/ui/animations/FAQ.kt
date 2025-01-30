cae1ba793d06995d48bfd539bb8394558b0492ee
Marcin Moskała <marcinmoskala@gmail.com>
2025-01-30 10:05:03 +0100

    Update FAQ exercise

--- app/src/main/java/com/marcinmoskala/composeexercises/ui/animations/FAQ.kt
+++ app/src/main/java/com/marcinmoskala/composeexercises/ui/animations/FAQ.kt
@@ -1,5 +1,8 @@
 package com.marcinmoskala.composeexercises.ui.animations
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.style.TextOverflow
@@ -29,7 +34,9 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 private fun FrequentlyAskedQuestionsPage() {
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp)
+    ) {
         faq.forEach { (question, answer) ->
             item {
                 FAQItem(question, answer)
@@ -47,14 +54,15 @@ private val faq = mapOf(
 
 @Composable
 private fun FAQItem(question: String, answer: String) {
-    var opened by remember { mutableStateOf(false) }
+    var isOpen by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { opened = !opened }
+            .clickable { isOpen = !isOpen }
             .padding(4.dp)
     ) {
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(8.dp)
         ) {
             Row(
@@ -71,15 +79,21 @@ private fun FAQItem(question: String, answer: String) {
                     contentDescription = "Expand",
                     modifier = Modifier
                         .size(24.dp)
-                        .rotate(if (opened) 180f else 0f)
+                        .rotate(if (isOpen) 180f else 0f)
                 )
             }
             Text(
                 text = answer,
                 fontSize = 14.sp,
-                maxLines = if (opened) Int.MAX_VALUE else 2,
+                maxLines = if (isOpen) Int.MAX_VALUE else 2,
                 overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(top = 4.dp)
             )
+            if(isOpen) {
+                Button(onClick = {}, modifier = Modifier.padding(top = 4.dp)) {
+                    Text("Need more help? Contact us")
+                }
+            }
         }
     }
 }
