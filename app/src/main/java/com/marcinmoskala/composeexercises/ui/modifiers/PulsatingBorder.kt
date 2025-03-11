ae56a0889c28fe00c2f18b5371f4a01d46a1cc06
marcinmoskala <marcinmoskala@gmail.com>
2025-03-11 10:40:20 +0100

    Correct PulsatingButtonPreview

--- app/src/main/java/com/marcinmoskala/composeexercises/ui/modifiers/PulsatingBorder.kt
+++ app/src/main/java/com/marcinmoskala/composeexercises/ui/modifiers/PulsatingBorder.kt
@@ -1,9 +1,11 @@
 package com.marcinmoskala.composeexercises.ui.modifiers
 
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -19,14 +21,15 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun PulsatingButtonPreview() {
-    val animatedColor by animateColorAsState(
-        targetValue = Color.Blue,
-        animationSpec = infiniteRepeatable(
-            animation = tween(2000, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        ),
-        label = "pulsatingBorderColor"
-    )
+    val animatedColor by rememberInfiniteTransition()
+        .animateColor(
+            initialValue = Color.Blue,
+            targetValue = Color.Blue.copy(alpha = 0.3f),
+            animationSpec = infiniteRepeatable(
+                animation = tween(2000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
 
     Text(
         text = "Click me",
@@ -34,7 +37,7 @@ fun PulsatingButtonPreview() {
             .padding(8.dp)
             .drawBehind {
                 drawRect(
-                    color = animatedColor.copy(alpha = 0.3f),
+                    color = animatedColor,
                     style = Stroke(width = 4.dp.toPx()),
                     size = size
                 )
