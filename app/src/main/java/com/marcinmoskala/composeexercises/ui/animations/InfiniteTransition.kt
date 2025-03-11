f26dfe2adeb15939cd0761ef682ae55684e7aea2
marcinmoskala <marcinmoskala@gmail.com>
2025-03-11 14:33:01 +0100

    Correct InfiniteTransition.kt

---
+++ app/src/main/java/com/marcinmoskala/composeexercises/ui/animations/InfiniteTransition.kt
@@ -0,0 +1,36 @@
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+private fun InfiniteTransitionExample() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "color"
+    )
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = color)
+    )
+}
