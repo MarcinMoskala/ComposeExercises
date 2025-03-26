9002baa44467a35a873941f74f9e92378e4f18e1
Marcin Moskała <marcinmoskala@gmail.com>
2025-03-26 12:55:24 +0100

    Clean Contacts exercise template

--- app/src/main/java/com/marcinmoskala/composeexercises/ui/basics/ContactsApp.kt
+++ app/src/main/java/com/marcinmoskala/composeexercises/ui/basics/ContactsApp.kt
@@ -4,41 +4,23 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.marcinmoskala.composeexercises.R
 
 class ContactsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ContactsApplicationTheme {
-                ContactsScreen()
-            }
+            ContactsScreen()
         }
     }
 }
@@ -55,93 +37,62 @@ private fun ContactsScreen() {
 
 @Composable
 private fun ContactItem(contact: Contact) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.avatar),
-            contentDescription = "Contact avatar",
-            modifier = Modifier
-                .size(60.dp)
-                .padding(8.dp)
-                .clip(shape = CircleShape)
-        )
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text(
-                contact.fillName,
-                style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.primary
-            )
-            Text(
-                contact.label,
-                style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.secondary
-            )
+    // TODO
+}
+
+@Composable
+private fun AddContactDialog(
+    onAddItem: (Contact) -> Unit,
+    onDismiss: () -> Unit
+) {
+    var fillName by remember { mutableStateOf("") }
+    var label by remember { mutableStateOf("") }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(text = "Add New Item") },
+        text = {
+            Column {
+                TextField(
+                    value = fillName,
+                    onValueChange = { fillName = it },
+                    label = { Text("Full name") }
+                )
+                TextField(
+                    value = label,
+                    onValueChange = { label = it },
+                    label = { Text("Label") }
+                )
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onAddItem(Contact(fillName, label))
+                    onDismiss()
+                }
+            ) {
+                Text("Add")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Cancel")
+            }
         }
-    }
+    )
 }
 
 @Preview
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun PreviewContactItem() {
-    ContactsApplicationTheme {
-        ContactItem(Contact("John Doe", "Friend"))
-    }
+fun ContactItemPreview() {
+    ContactItem(Contact("John Doe", "Friend"))
 }
 
-
-// Theme files
-val Blue = Color(0xFF2E90CF)
-val Black = Color(0xFF000000)
-val Orange = Color(0xFFFFA621)
-
-val BlueOnDark = Color(0xFF91cdff)
-val BlackOnDark = Color(0xffc6c6c6)
-val OrangeOnDark = Color(0xffffcc92)
-
-val Typography = Typography(
-    titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 20.sp
-    ),
-    labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    )
-)
-
+@Preview
 @Composable
-fun ContactsApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        darkTheme -> darkColorScheme(
-            primary = BlueOnDark,
-            secondary = BlackOnDark,
-            tertiary = OrangeOnDark
-        )
-
-        else -> lightColorScheme(
-            primary = Blue,
-            secondary = Black,
-            tertiary = Orange
-        )
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        shapes = MaterialTheme.shapes,
-        typography = Typography,
-        content = content
-    )
+private fun AddContactDialogPreview() {
+    AddContactDialog({}, {})
 }
